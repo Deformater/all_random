ebedf4c495e40387913e24b3155861a4082911d7
Deformater <76251290+deformater@users.noreply.github.com>
2021-12-28 17:18:20 +0300

    Generate password

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from flask import Flask, render_template, request
+from flask_cors import CORS
+from werkzeug.security import generate_password_hash
+from settings import HOST
+import requests
+
 
 app = Flask(__name__)
+CORS(app)
+app.config['SECRET_KEY'] = 'secret_key'
 
 
 @app.route("/")
@@ -16,10 +23,26 @@ def coin():
     return render_template('coin.html', nav_item_active_dict=nav_item_active_dict)
 
 
-@app.route("/password")
+@app.route("/password", methods=['GET', 'POST'])
 def password():
     nav_item_active_dict = {'index': '', 'coin': '', 'password': 'active'}
-    return render_template('password.html', nav_item_active_dict=nav_item_active_dict)
+    if request.method == 'GET':
+        return render_template('password.html', nav_item_active_dict=nav_item_active_dict, password='dfgd')
+    elif request.method == 'POST':
+        data_json = request.json
+        length = data_json['length']
+        is_number = data_json['numbers']
+        is_spec_symbol = data_json['spec_symbols']
+
+        # response = requests.get(f'{HOST}/password?length={length}&is_number={is_number}'
+        #                         f'&is_spec_symbol={is_spec_symbol}')
+        # if response.status_code != 200:
+        #     return render_template('password.html', error=response.text)
+        #
+        # password = response.json()['password']
+
+        password = generate_password_hash('aboba')
+        return render_template('password.html', password=password, nav_item_active_dict=nav_item_active_dict)
 
 
 if __name__ == "__main__":
---
+++ static/app.js
@@ -0,0 +1,43 @@
+const host = 'http://0.0.0.0:5050'
+let passButton = document.getElementById('generate_psw_btn')
+passButton.onclick = async function (){
+    let number_switch = document.getElementById('numbers_switch').querySelector('input')
+    let spec_symbols = document.getElementById('spec_symbols_switch').querySelector('input')
+    let length = document.getElementById('length_input')
+
+    const url = host + `/password?length=${length.value}&is_number=${number_switch.checked}&is_spec_symbol=${spec_symbols.checked}`
+
+    try {
+        const response = await fetch(url, {
+            method: 'GET',
+        })
+        // let data = response.json()
+        // let pass = data['password']
+        let psw_input = document.getElementById('psw_input')
+        let safety = 100
+        if (length.value <= 15)
+            safety -= 50 - length.value * 3
+        if (!number_switch.checked)
+            safety -= 20
+        if (!spec_symbols.checked)
+            safety -= 20
+        if (safety < 0)
+            safety = 0
+        if (safety > 100)
+            safety = 100
+
+        let psw_safety = document.getElementById('psw_safety_div')
+        psw_safety.style.width = safety + '%'
+        console.log(safety)
+        if (safety > 0 && safety < 40)
+            psw_safety.style.background = 'red'
+        if (safety >= 40 && safety < 75)
+            psw_safety.style.background = 'yellow'
+        if (safety >= 75 && safety <= 100)
+            psw_safety.style.background = 'green'
+
+        psw_input.value = 'alkfgalsdmka;slkg'
+    } catch (error) {
+        console.error('Ошибка:', error)
+    }
+}
