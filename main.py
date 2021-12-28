4baf90bc9d5ba80182b758eeef8b9e64e7171cf6
Deformater <76251290+deformater@users.noreply.github.com>
2021-12-28 13:35:31 +0300

    Working redirect without js

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template, request
+
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello():
-    return "<h1 style='color:blue'>Hello There!</h1>"
+@app.route("/index")
+def index():
+    nav_item_active_dict = {'index': 'active', 'coin': '', 'password': ''}
+    return render_template('index.html', nav_item_active_dict=nav_item_active_dict)
+
+
+@app.route("/coin")
+def coin():
+    nav_item_active_dict = {'index': '', 'coin': 'active', 'password': ''}
+    return render_template('coin.html', nav_item_active_dict=nav_item_active_dict)
+
+
+@app.route("/password")
+def password():
+    nav_item_active_dict = {'index': '', 'coin': '', 'password': 'active'}
+    return render_template('password.html', nav_item_active_dict=nav_item_active_dict)
 
 
 if __name__ == "__main__":
